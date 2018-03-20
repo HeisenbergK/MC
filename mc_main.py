8d08700c70c17c1f334f64bd3ef44b0753b72594
John Andrew Kypriotakis <johnykypriotakis@gmail.com>
2018-03-20 19:36:59 +0530

    added montecarlo for more input distributions

--- mc_main.py
+++ mc_main.py
@@ -13,3 +13,30 @@ def montecarlo(probe, inputed, limit):
         result = probe(inputsrand)
         results.append(result)
     return results
+
+
+def montecarlolaplace(probe, inputed, limit):
+    results = []
+    for i in range(1, (limit+1)):
+        inputsrand = []
+        for j in range(0, len(inputed)):
+            locat = inputed[j][0]
+            stdev = inputed[j][1]
+            value = np.random.laplace(loc=locat, scale=stdev)
+            inputsrand.append(value)
+        result = probe(inputsrand)
+        results.append(result)
+    return results
+
+
+def montecarlopoisson(probe, inputed, limit):
+    results = []
+    for i in range(1, (limit+1)):
+        inputsrand = []
+        for j in range(0, len(inputed)):
+            locat = inputed[j][0]
+            value = np.random.poisson(lam=locat)
+            inputsrand.append(value)
+        result = probe(inputsrand)
+        results.append(result)
+    return results
