ad62badd2abf1054ff0a8fda6287e85ccac05ae1
John Andrew Kypriotakis <johnykypriotakis@gmail.com>
2018-03-22 18:43:49 +0530

    added more trinkets

--- mc_main.py
+++ mc_main.py
@@ -25,33 +25,40 @@ def montecarlo(probe, inputed, limit, llimit, ulimit):
 
 def montecarlolaplace(probe, inputed, limit, llimit, ulimit):
     results = []
-    for i in range(1, (limit+1)):
-        inputsrand = []
-        for j in range(0, len(inputed)):
-            locat = inputed[j][0]
-            stdev = inputed[j][1]
-            while True:
-                value = np.random.laplace(loc=locat, scale=stdev)
-                if llimit < value < ulimit:
-                    inputsrand.append(value)
-                    break
-        result = probe(inputsrand)
-        results.append(result)
+    with progressbar.ProgressBar(widgets=[progressbar.Bar(), ' ', progressbar.RotatingMarker(), ' ',
+                                          progressbar.Percentage(), ' ', progressbar.SimpleProgress(), ' ',
+                                          progressbar.Timer(), ' ', progressbar.ETA()], max_value=limit) as bar:
+        for i in range(0, limit):
+            inputsrand = []
+            for j in range(0, len(inputed)):
+                locat = inputed[j][0]
+                stdev = inputed[j][1]
+                while True:
+                    value = np.random.laplace(loc=locat, scale=stdev)
+                    if llimit < value < ulimit:
+                        inputsrand.append(value)
+                        break
+            result = probe(inputsrand)
+            results.append(result)
+            bar.update(i)
     return results
 
 
 def montecarlopoisson(probe, inputed, limit, llimit, ulimit):
     results = []
-    for i in range(1, (limit+1)):
-        inputsrand = []
-        for j in range(0, len(inputed)):
-            locat = inputed[j][0]
-            stdev = inputed[j][1]
-            while True:
-                value = np.random.poisson(lam=locat)
-                if llimit < value < ulimit:
-                    inputsrand.append(value)
-                    break
-        result = probe(inputsrand)
-        results.append(result)
+    with progressbar.ProgressBar(widgets=[progressbar.Bar(), ' ', progressbar.RotatingMarker(), ' ',
+                                          progressbar.Percentage(), ' ', progressbar.SimpleProgress(), ' ',
+                                          progressbar.Timer(), ' ', progressbar.ETA()], max_value=limit) as bar:
+        for i in range(0, limit):
+            inputsrand = []
+            for j in range(0, len(inputed)):
+                locat = inputed[j][0]
+                while True:
+                    value = np.random.poisson(lam=locat)
+                    if llimit < value < ulimit:
+                        inputsrand.append(value)
+                        break
+            result = probe(inputsrand)
+            results.append(result)
+            bar.update(i)
     return results
